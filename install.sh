6bffacef3dcb098d7f8b954ecc0be464aec8098e
Jinghui Hu <hujinghui@buaa.edu.cn>
2017-11-20 22:16:27 +0800

    change install scripts

--- install.sh
+++ install.sh
@@ -22,49 +22,49 @@ HERE=`dirname $(realpath $0)`
 
 __symbolic_link_file() {
     [ $# -ne 2 ] && return ;
-    local target="$HERE/$1"
-    local source="$HOME/$2"
-    if [ -f $target ] && [ ! -e $source ]; then
-        echo "ln -s $target $source"
-        ln -s $target $source
+    local src="$HOME/$1"
+    local des="$HERE/$2"
+    if [ -f $des ] && [ ! -e $src ]; then
+        echo "ln -s $des $src"
+        ln -s $des $src
     fi
 }
 
 __symbolic_link_folder() {
     [ $# -ne 2 ] && return ;
-    local target="$HERE/$1"
-    local source="$HOME/$2"
-    if [ -d $target ] && [ ! -e $source ]; then
-        echo "ln -s $target $source"
-        ln -s $target $source
+    local src="$HOME/$1"
+    local des="$HERE/$2"
+    if [ -d $des ] && [ ! -e $src ]; then
+        echo "ln -s $des $src"
+        ln -s $des $src
     fi
 }
 
 
 # bash
-__symbolic_link_folder 'bash' '.bash'
+__symbolic_link_folder '.bash' 'bash'
 
 # zsh
-__symbolic_link_folder 'zsh' '.zsh'
+__symbolic_link_folder '.zsh' 'zsh'
 
 # vim
-__symbolic_link_file 'vim/vimrc' '.vimrc'
-__symbolic_link_folder 'vim' '.vim'
+__symbolic_link_file '.vimrc' 'vim/vimrc'
+__symbolic_link_folder '.vim' 'vim'
 
 # git
-__symbolic_link_file 'git/gitconfig' '.gitconfig'
-__symbolic_link_file 'git/gitignore_global' '.gitignore_global'
-__symbolic_link_file 'git/gitattributes_global' '.gitattributes_global'
+__symbolic_link_file '.gitconfig' 'git/gitconfig'
+__symbolic_link_file '.gitignore_global' 'git/gitignore_global'
+__symbolic_link_file '.gitattributes_global' 'git/gitattributes_global'
 
 # tmux
-__symbolic_link_file 'tmux.conf' '.tmux.conf'
+__symbolic_link_file '.tmux.conf' 'tmux.conf'
 
 # readline
-__symbolic_link_file 'inputrc' '.inputrc'
-__symbolic_link_file 'curlrc' '.curlrc'
+__symbolic_link_file '.inputrc' 'inputrc'
+__symbolic_link_file '.curlrc' 'curlrc'
 
 # ctags
-__symbolic_link_file 'ctags' '.ctags'
+__symbolic_link_file '.ctags' 'ctags'
 
 # mplayer
-__symbolic_link_folder 'mplayer' '.mplayer'
+__symbolic_link_folder '.mplayer' 'mplayer'
