b8ad8fc85db2ecc97fddf469ddedf1624dd4be56
Jinghui Hu <hujinghui@buaa.edu.cn>
2017-11-20 20:01:26 +0800

    modify install script

--- install.sh
+++ install.sh
@@ -17,44 +17,29 @@
 #                                                                       #
 #########################################################################
 
-# basic function #
+HERE=`dirname $(realpath $0)`
 
-__current_dir() {
-    cd $(dirname "$0")
-    pwd -P
-}
-
-__home_dir() {
-    echo $HOME
-}
 
 __symbolic_link_file() {
     [ $# -ne 2 ] && return ;
-    local target_file=$1
-    local source_file=$2
-    local target=$(__current_dir)'/'$target_file
-    local source=$(__home_dir)'/'$source_file
+    local target="$HERE/$1"
+    local source="$HOME/$2"
     if [ -f $target ] && [ ! -e $source ]; then
-        echo "ln -s $target $source" && ln -s $target $source
+        echo "ln -s $target $source"
+        ln -s $target $source
     fi
 }
 
 __symbolic_link_folder() {
     [ $# -ne 2 ] && return ;
-    local target_file=$1
-    local source_file=$2
-    local target=$(__current_dir)'/'$target_file
-    local source=$(__home_dir)'/'$source_file
+    local target="$HERE/$1"
+    local source="$HOME/$2"
     if [ -d $target ] && [ ! -e $source ]; then
-        echo "ln -s $target $source" && ln -s $target $source
+        echo "ln -s $target $source"
+        ln -s $target $source
     fi
 }
 
-########## do link work
-
-# readline
-__symbolic_link_file 'inputrc' '.inputrc'
-__symbolic_link_file 'curlrc' '.curlrc'
 
 # bash
 __symbolic_link_folder 'bash' '.bash'
@@ -62,17 +47,21 @@ __symbolic_link_folder 'bash' '.bash'
 # zsh
 __symbolic_link_folder 'zsh' '.zsh'
 
+# vim
+__symbolic_link_file 'vim/vimrc' '.vimrc'
+__symbolic_link_folder 'vim' '.vim'
+
 # git
 __symbolic_link_file 'git/gitconfig' '.gitconfig'
 __symbolic_link_file 'git/gitignore_global' '.gitignore_global'
 __symbolic_link_file 'git/gitattributes_global' '.gitattributes_global'
 
-# vim
-__symbolic_link_file 'vim/vimrc' '.vimrc'
-__symbolic_link_folder 'vim' '.vim'
-
 # tmux
 __symbolic_link_file 'tmux.conf' '.tmux.conf'
+
+# readline
+__symbolic_link_file 'inputrc' '.inputrc'
+__symbolic_link_file 'curlrc' '.curlrc'
 
 # ctags
 __symbolic_link_file 'ctags' '.ctags'
