bbe19cc60a2718499ff17b1cfb7027f3c2db607b
Jinghui Hu <hujinghui@buaa.edu.cn>
2017-11-20 20:35:12 +0800

    rewrite color tab for iterm2

--- zsh/autoload/iterm2.sh
+++ zsh/autoload/iterm2.sh
@@ -19,30 +19,30 @@
 # iTerm2 window/tab color commands
 #   Requires iTerm2 >= Build 1.0.0.20110804
 #   http://code.google.com/p/iterm2/wiki/ProprietaryEscapeCodes
-tab-color() {
+_setrgb() {
     echo -ne "\033]6;1;bg;red;brightness;$1\a"
     echo -ne "\033]6;1;bg;green;brightness;$2\a"
     echo -ne "\033]6;1;bg;blue;brightness;$3\a"
 }
-tab-reset() {
+
+_reset() {
     echo -ne "\033]6;1;bg;*;default\a"
 }
 
-# Change the color of the tab when using SSH
-# reset the color after the connection closes
-color-ssh() {
-    if [[ -n "$ITERM_SESSION_ID" ]]; then
-        trap "tab-reset" INT EXIT
+# Change the color of the tab when using SSH, reset the color after the connection closes
+_colorssh() {
+    if [ -n "$ITERM_SESSION_ID" ]; then
+        trap "_reset" INT EXIT
         if [[ "$*" =~ ".*lejent\.cn$" ]]; then
-            tab-color 144 238 144
-        elif [[ "$*" =~ "hp*" ]]; then
-            tab-color 0 104 139
+            _setrgb 144 238 144
+        elif [ "$*" =~ "hp*" ]; then
+            _setrgb 0 104 139
         else
-            tab-color 255 110 180
+            _setrgb 255 110 180
         fi
     fi
     ssh $*
 }
-compdef _ssh color-ssh=ssh
+compdef _ssh _colorssh=ssh
 
-alias ssh=color-ssh
+alias ssh=_colorssh
