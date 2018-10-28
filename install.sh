0210816cc2364afc93aa9deb9974595ce1836d2b
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-10-28 22:50:11 +0800

    [update] install script

--- install.sh
+++ install.sh
@@ -1,15 +1,28 @@
 #!/usr/bin/env sh
-HERE=`cd $(dirname $0); pwd`
 
+# install homebrew
+if [ ! -x "/usr/local/bin/brew" ]; then
+  open "https://brew.sh"
+  # /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  exit 1
+fi
+
+# install oh-my-zsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  open "https://ohmyz.sh"
+  # sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  exit 1
+fi
+
+# setup dotfiles
 export DOTFILES="$HOME/.dotfiles"
-
 if [ ! -d $DOTFILES ]; then
   git clone https://github.com/Jeanhwea/dotfiles.git $DOTFILES
 fi
 
 __linkfile() {
   local src="$HOME/$1"
-  local des="$HERE/$1"
+  local des="$DOTFILES/$1"
   if [ -f $des ] && [ ! -e $src ]; then
     echo "ln -s $des $src"
     ln -s $des $src
@@ -18,7 +31,7 @@ __linkfile() {
 
 __linkdir() {
   local src="$HOME/$1"
-  local des="$HERE/$1"
+  local des="$DOTFILES/$1"
   if [ -d $des ] && [ ! -e $src ]; then
     echo "ln -s $des $src"
     ln -s $des $src
@@ -32,13 +45,15 @@ __linkfile '.zshrc'
 # vim
 __linkfile '.vimrc'
 __linkdir '.vim'
+if [ ! -d "$HOME/.vim/bundle/vundle/" ]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git "$HOME/.vim/bundle/vundle/"
+fi
 
 # git
 __linkfile '.gitconfig'
 __linkfile '.gitignore_global'
 __linkfile '.gitattributes_global'
 __linkfile '.gitcommitmsg'
-
 
 # tmux
 __linkfile '.tmux.conf'
@@ -52,3 +67,24 @@ __linkfile '.ctags'
 
 # mplayer
 __linkdir '.mplayer'
+
+
+# install software
+
+brew update
+
+
+_mkdirp() {
+  local dir=$1
+  if [ ! -d $dir ]; then
+    mkdir -p $dir
+  fi
+}
+
+
+brew install coreutils moreutils
+brew install git tmux pandoc graphviz python3 ffmpeg mplayer htop node maven p7zip reattach-to-user-namespace springboot
+brew install tree unrar yarn you-get
+
+brew cask install iterm2
+brew cask install firefox
