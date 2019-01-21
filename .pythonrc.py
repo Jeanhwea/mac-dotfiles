fac83b049da6ede94152de4a8d088b93bf88cca3
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-01-21 22:05:50 +0800

    refactor codes

--- .pythonrc.py
+++ .pythonrc.py
@@ -6,13 +6,13 @@ import sys
 
 def _change_working_dir():
   ''' set python shell working path to git top directory. '''
-  work_dir = os.getcwd()
+  cwd = os.getcwd()
   while True:
-    if os.path.exists(os.path.join(work_dir, '.git')):
-      os.chdir(work_dir)
+    if os.path.exists(os.path.join(cwd, '.git')):
+      os.chdir(cwd)
       break
-    if work_dir != os.path.dirname(work_dir):
-      work_dir = os.path.dirname(work_dir)
+    if cwd != os.path.dirname(cwd):
+      cwd = os.path.dirname(cwd)
     else:
       break
   print('work directory -> {}'.format(os.getcwd()))
@@ -20,15 +20,11 @@ def _change_working_dir():
 
 def _insert_to_sys_path():
   ''' insert current work directory to sys.path. '''
-  cur_dir = os.getcwd()
-  sys.path.insert(-1, cur_dir)
-  # remove duplicate items
-  path_old = sys.path
-  sys.path = []
-  for p in path_old:
-    if not p or len(p) <= 0 or not os.path.exists(p) or p in sys.path:
-      continue
-    sys.path.append(p)
+  paths = sys.path
+  sys.path = [os.getcwd()]
+  for p in paths:
+    if p and len(p) > 0 and os.path.exists(p) and p not in sys.path:
+      sys.path.append(p)
   print('sys path -> {}'.format(sys.path))
 
 
