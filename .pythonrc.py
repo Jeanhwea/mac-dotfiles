458f5c178508cde3bc446b9d18e1741f0695cf44
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-01-21 09:25:42 +0800

    add more setup

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,16 +1,39 @@
 # -*- coding: utf-8 -*-
 # export PYTHONSTARTUP=$HOME/.pythonrc.py
 import os
+import sys
+
 
 # set python shell working path to git top directory
-__wdir = os.getcwd()
-while True:
-  if os.path.exists(os.path.join(__wdir, '.git')):
-    os.chdir(__wdir)
-    break
-  if __wdir != os.path.dirname(__wdir):
-    __wdir = os.path.dirname(__wdir)
-  else:
-    break
-# display a prompt message
-print('>>> os.chdir("{}")'.format(__wdir))
+def _change_working_dir():
+  work_dir = os.getcwd()
+  while True:
+    if os.path.exists(os.path.join(work_dir, '.git')):
+      os.chdir(work_dir)
+      break
+    if work_dir != os.path.dirname(work_dir):
+      work_dir = os.path.dirname(work_dir)
+    else:
+      break
+  print('work directory -> {}'.format(os.getcwd()))
+
+
+def _insert_to_sys_path():
+  # insert current work directory to sys.path
+  cur_dir = os.getcwd()
+  sys.path.insert(-1, cur_dir)
+  # remove duplicate items
+  path_set = set()
+  path_old = sys.path
+  sys.path = []
+  for p in path_old:
+    if p and p in path_set:
+      continue
+    path_set.add(p)
+    sys.path.append(p)
+  print('sys path -> {}'.format(sys.path))
+
+
+if __name__ == '__main__':
+  _change_working_dir()
+  _insert_to_sys_path()
