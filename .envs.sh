0917f3e75c1f61e9a4e7aa22a630685d2099cf62
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-11-18 08:54:36 +0800

    给环境变量分节

--- .envs.sh
+++ .envs.sh
@@ -1,13 +1,22 @@
-# let python don't write byte code
-export PYTHONDONTWRITEBYTECODE=1
+################################################################################
+# Section 1: Comfortable Environment
 
-# change homebrew bottle domain
-export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
+# let git log page more friendly
+export LESS="-F -X $LESS"
 
 # setup zsh-autosuggestions.zsh
 # 1. git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # 2. add plugins=(zsh-autosuggestions) to ~/.zshrc
 export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=yellow'
+
+# let python don't write byte code
+export PYTHONDONTWRITEBYTECODE=1
+
+################################################################################
+# Section 2: Better Download Experience
+
+# change homebrew bottle domain
+export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
 
 # taobao mirror for npm
 export NVM_NODEJS_ORG_MIRROR=http://npm.taobao.org/mirrors/node
@@ -15,9 +24,9 @@ export SASS_BINARY_SITE=http://npm.taobao.org/mirrors/node-sass
 export ELECTRON_MIRROR=http://npm.taobao.org/mirrors/electron/
 export SQLITE3_BINARY_SITE=http://npm.taobao.org/mirrors/sqlite3
 
+################################################################################
+# Section 3: Localization
+
 # setup sqlplus to chinese simplified
 export NLS_LANG="AMERICAN_AMERICA.AL32UTF8"
 export NLS_LANG="SIMPLIFIED CHINESE_CHINA.AL32UTF8"
-
-# let git log page more friendly
-export LESS="-F -X $LESS"
