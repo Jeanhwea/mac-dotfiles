35536acaeda5b33b12410135a2323184c6c444fb
Jinghui Hu <hujinghui@buaa.edu.cn>
2020-02-08 22:52:50 +0800

    disable lantern

--- install-mac.sh
+++ install-mac.sh
@@ -127,7 +127,7 @@ __brew_cask_install emacs
 __brew_cask_install foxmail
 __brew_cask_install google-chrome
 __brew_cask_install iterm2
-__brew_cask_install lantern
+# __brew_cask_install lantern
 __brew_cask_install macpass
 __brew_cask_install mactex
 __brew_cask_install macvim
